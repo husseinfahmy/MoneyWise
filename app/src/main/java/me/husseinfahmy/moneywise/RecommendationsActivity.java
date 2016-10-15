2c6706162455cae27c4fd6efbfc79b345dc9d852
Hussein Fahmy <fahmy.hussein@hotmail.com>
2016-10-15 15:09:47 -0400

    recommendations activity and setup of yelp api

---
+++ app/src/main/java/me/husseinfahmy/moneywise/RecommendationsActivity.java
@@ -0,0 +1,19 @@
+package me.husseinfahmy.moneywise;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.yelp.clientlib.connection.YelpAPI;
+import com.yelp.clientlib.connection.YelpAPIFactory;
+
+public class RecommendationsActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_recommendations);
+
+        YelpAPIFactory apiFactory = new YelpAPIFactory(Mas47GwCA1Bio3uJ08yIGg, o9rKaeLjjopt_90o1bdGFbuIbmc, XlzyPKV4L9QFzaaw6qr3FO1zala2F6NL, In5IaMHhjtc4Gz4Pv6Fs1A3paPU);
+        YelpAPI yelpAPI = apiFactory.createAPI();
+    }
+}
