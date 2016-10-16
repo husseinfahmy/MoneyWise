b6c78a5b1db86173047a04e0cdcf508d80881f6b
Hussein Fahmy <fahmy.hussein@hotmail.com>
2016-10-15 21:34:01 -0400

    changed startup activity to tabbed activity

--- app/src/main/java/me/husseinfahmy/moneywise/RecommendationsActivity.java
+++ app/src/main/java/me/husseinfahmy/moneywise/RecommendationsActivity.java
@@ -6,6 +6,9 @@ import android.os.Bundle;
 import com.yelp.clientlib.connection.YelpAPI;
 import com.yelp.clientlib.connection.YelpAPIFactory;
 
+import java.util.HashMap;
+import java.util.Map;
+
 public class RecommendationsActivity extends AppCompatActivity {
 
     @Override
@@ -13,7 +16,12 @@ public class RecommendationsActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_recommendations);
 
-        YelpAPIFactory apiFactory = new YelpAPIFactory(Mas47GwCA1Bio3uJ08yIGg, o9rKaeLjjopt_90o1bdGFbuIbmc, XlzyPKV4L9QFzaaw6qr3FO1zala2F6NL, In5IaMHhjtc4Gz4Pv6Fs1A3paPU);
+        YelpAPIFactory apiFactory = new YelpAPIFactory("Mas47GwCA1Bio3uJ08yIGg", "o9rKaeLjjopt_90o1bdGFbuIbmc", "XlzyPKV4L9QFzaaw6qr3FO1zala2F6NL", "In5IaMHhjtc4Gz4Pv6Fs1A3paPU");
         YelpAPI yelpAPI = apiFactory.createAPI();
+        Map<String, String> params = new HashMap<>();
+
+        params.put("term","restaurant");
+        params.put("limit", "10");
+
     }
 }
